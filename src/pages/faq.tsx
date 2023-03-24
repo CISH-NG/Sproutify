d3e097ed22ea79120f41b088306552ab8098aa5f
Ufoma Ibukunoluwa Odufejo <ufomaodufejo@gmail.com>
2023-03-24 09:41:25 +0100

    Making the faq cleaner by not repeating the code.

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,68 +1,41 @@
 import DefaultLayout from '@/components/layouts/DefaultLayout';
+import FaqLayout from '@/components/layouts/FaqLayout';
 import React from 'react';
 
 function faq() {
-  return (
-    <DefaultLayout>
-    <div className='w-5/6 mx-auto flex flex-col'>
-        <h1 className='text-3xl font-extrabold mt-5 text-center'>FAQs</h1>
-        <div className='mt-5'>
-            <h1 className='text-xl font-medium ml-5 p-3 rounded-xl text-white bg-[#4C2884]'>General</h1>
-            <div className='ml-10'>
-                <h1 className='text-lg font-medium ml-5 mt-5 mb-2'>Who are we?</h1>
-            </div> <hr className='ml-12'></hr>
-            <div className='ml-10'>
-                <h1 className='text-lg font-medium ml-5 mt-5 mb-2'>What do we do?</h1>
-            </div> <hr className='ml-12'></hr>
-            <div className='ml-10'>
-                <h1 className='text-lg font-medium ml-5 mt-5 mb-2'>How long have we existed?</h1>
-            </div> <hr className='ml-12'></hr>
-            <div className='ml-10'>
-                <h1 className='text-lg font-medium ml-5 mt-5 mb-2'>What benefits do i get from your service?</h1>
-            </div> <hr className='ml-12'></hr>
-            <div className='ml-10'>
-                <h1 className='text-lg font-medium ml-5 mt-5 mb-2'>Who are we?</h1>
-            </div> <hr className='ml-12'></hr>
-        </div>
-        <div className='mt-5'>
-            <h1 className='text-xl font-medium ml-5 border-solid bg-[#4C2884] p-3 rounded-xl text-white'>Privacy</h1>
-            <div className='ml-10'>
-                <h1 className='text-lg font-medium ml-5 mt-5 mb-2'>Who are we?</h1>
-            </div> <hr className='ml-12'></hr>
-            <div className='ml-10'>
-                <h1 className='text-lg font-medium ml-5 mt-5 mb-2'>What do we do?</h1>
-            </div> <hr className='ml-12'></hr>
-            <div className='ml-10'>
-                <h1 className='text-lg font-medium ml-5 mt-5 mb-2'>How long have we existed?</h1>
-            </div> <hr className='ml-12'></hr>
-            <div className='ml-10'>
-                <h1 className='text-lg font-medium ml-5 mt-5 mb-2'>What benefits do i get from your service?</h1>
-            </div> <hr className='ml-12'></hr>
-            <div className='ml-10'>
-                <h1 className='text-lg font-medium ml-5 mt-5 mb-2'>Who are we?</h1>
-            </div> <hr className='ml-12'></hr>
-        </div>
-        <div className='mt-5'>
-            <h1 className='text-xl font-medium ml-5 border-solid bg-[#4C2884] p-3 rounded-xl text-white'>Technical</h1>
-            <div className='ml-10'>
-                <h1 className='text-lg font-medium ml-5 mt-5 mb-2'>Who are we?</h1>
-            </div> <hr className='ml-12'></hr>
-            <div className='ml-10'>
-                <h1 className='text-lg font-medium ml-5 mt-5 mb-2'>What do we do?</h1>
-            </div> <hr className='ml-12'></hr>
-            <div className='ml-10'>
-                <h1 className='text-lg font-medium ml-5 mt-5 mb-2'>How long have we existed?</h1>
-            </div> <hr className='ml-12'></hr>
-            <div className='ml-10'>
-                <h1 className='text-lg font-medium ml-5 mt-5 mb-2'>What benefits do i get from your service?</h1>
-            </div> <hr className='ml-12'></hr>
-            <div className='ml-10'>
-                <h1 className='text-lg font-medium ml-5 mt-5 mb-2'>Who are we?</h1>
-            </div> <hr className='ml-12'></hr>
-        </div>
-    </div>
-    </DefaultLayout>
-  );
+	return (
+		<DefaultLayout>
+			<div className="mx-auto flex w-5/6 flex-col">
+				<h1 className="mt-5 text-center text-3xl font-extrabold">FAQs</h1>
+				<FaqLayout 
+                title="General" 
+                question1= "Who are we?"
+                question2= "What do we do?"
+                question3= "How long have we existed?"
+                content3= "Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas rhoncus est magna euismod in dolor risus. Morbi nulla feugiat sit orci. Ac et velit sed phasellus mattis. Mauris imperdiet aenean convallis aliquet. Nunc vestibulum accumsan suspendisse dignissim magna. Sit cursus augue tortor mattis. Nullam sed ac tincidunt cras non"
+                question4 ="What benefits do i get from your service?"
+                question5 = "Who are we?"/>
+
+                <FaqLayout 
+                title="Privacy" 
+                question1= "Who are we?"
+                question2= "What do we do?"
+                question3= "How long have we existed?"
+                content3= "Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas rhoncus est magna euismod in dolor risus. Morbi nulla feugiat sit orci. Ac et velit sed phasellus mattis. Mauris imperdiet aenean convallis aliquet. Nunc vestibulum accumsan suspendisse dignissim magna. Sit cursus augue tortor mattis. Nullam sed ac tincidunt cras non"
+                question4 ="What benefits do i get from your service?"
+                question5 = "Who are we?"/>
+
+               <FaqLayout 
+                title="Technical" 
+                question1= "Who are we?"
+                question2= "What do we do?"
+                question3= "How long have we existed?"
+                content3= "Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas rhoncus est magna euismod in dolor risus. Morbi nulla feugiat sit orci. Ac et velit sed phasellus mattis. Mauris imperdiet aenean convallis aliquet. Nunc vestibulum accumsan suspendisse dignissim magna. Sit cursus augue tortor mattis. Nullam sed ac tincidunt cras non"
+                question4 ="What benefits do i get from your service?"
+                question5 = "Who are we?"/>
+			</div>
+		</DefaultLayout>
+	);
 }
 
 export default faq;
