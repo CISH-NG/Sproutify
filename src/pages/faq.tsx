c0f5e3949ecf61da7898687c8a97cb56938c0ade
Ojonugwa Alikali <alikali.ojonugwajustice@gmail.com>
2023-04-29 11:19:26 +0100

    Re-implement FAQs page

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,57 +1,19 @@
-import DefaultLayout from '@/layouts/DefaultLayout';
-import FaqComponent from '@/components/FaqComponent';
 import React from 'react';
+import DefaultLayout from '@/layouts/DefaultLayout';
+import FAQ from '@/screens/FAQ/FAQ';
 
-function faq() {
+function FaqPage() {
 	return (
 		<DefaultLayout>
 			<div className="mx-auto flex w-5/6 flex-col">
-				<h1 className="mt-5 text-center text-3xl font-extrabold">FAQs</h1>
+				<h1 className="mt-20 text-center text-3xl font-extrabold lg:mt-40">
+					FAQs
+				</h1>
 
-				<FaqComponent
-					title="General"
-					question1="Who are we?"
-					content1="Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas rhoncus est magna euismod in dolor risus. Morbi nulla feugiat sit orci. Ac et velit sed phasellus mattis. Mauris imperdiet aenean convallis aliquet. Nunc vestibulum accumsan suspendisse dignissim magna. Sit cursus augue tortor mattis. Nullam sed ac tincidunt cras non"
-					question2="What do we do?"
-					content2="Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas rhoncus est magna euismod in dolor risus. Morbi nulla feugiat sit orci. Ac et velit sed phasellus mattis. Mauris imperdiet aenean convallis aliquet. Nunc vestibulum accumsan suspendisse dignissim magna. Sit cursus augue tortor mattis. Nullam sed ac tincidunt cras non"
-					question3="How long have we existed?"
-					content3="Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas rhoncus est magna euismod in dolor risus. Morbi nulla feugiat sit orci. Ac et velit sed phasellus mattis. Mauris imperdiet aenean convallis aliquet. Nunc vestibulum accumsan suspendisse dignissim magna. Sit cursus augue tortor mattis. Nullam sed ac tincidunt cras non"
-					question4="What benefits do i get from your service?"
-					content4="Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas rhoncus est magna euismod in dolor risus. Morbi nulla feugiat sit orci. Ac et velit sed phasellus mattis. Mauris imperdiet aenean convallis aliquet. Nunc vestibulum accumsan suspendisse dignissim magna. Sit cursus augue tortor mattis. Nullam sed ac tincidunt cras non"
-					question5="Who are we?"
-					content5="Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas rhoncus est magna euismod in dolor risus. Morbi nulla feugiat sit orci. Ac et velit sed phasellus mattis. Mauris imperdiet aenean convallis aliquet. Nunc vestibulum accumsan suspendisse dignissim magna. Sit cursus augue tortor mattis. Nullam sed ac tincidunt cras non"
-				/>
-
-				<FaqComponent
-					title="Privacy"
-					question1="Who are we?"
-					content1="Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas rhoncus est magna euismod in dolor risus. Morbi nulla feugiat sit orci. Ac et velit sed phasellus mattis. Mauris imperdiet aenean convallis aliquet. Nunc vestibulum accumsan suspendisse dignissim magna. Sit cursus augue tortor mattis. Nullam sed ac tincidunt cras non"
-					question2="What do we do?"
-					content2="Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas rhoncus est magna euismod in dolor risus. Morbi nulla feugiat sit orci. Ac et velit sed phasellus mattis. Mauris imperdiet aenean convallis aliquet. Nunc vestibulum accumsan suspendisse dignissim magna. Sit cursus augue tortor mattis. Nullam sed ac tincidunt cras non"
-					question3="How long have we existed?"
-					content3="Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas rhoncus est magna euismod in dolor risus. Morbi nulla feugiat sit orci. Ac et velit sed phasellus mattis. Mauris imperdiet aenean convallis aliquet. Nunc vestibulum accumsan suspendisse dignissim magna. Sit cursus augue tortor mattis. Nullam sed ac tincidunt cras non"
-					question4="What benefits do i get from your service?"
-					content4="Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas rhoncus est magna euismod in dolor risus. Morbi nulla feugiat sit orci. Ac et velit sed phasellus mattis. Mauris imperdiet aenean convallis aliquet. Nunc vestibulum accumsan suspendisse dignissim magna. Sit cursus augue tortor mattis. Nullam sed ac tincidunt cras non"
-					question5="Who are we?"
-					content5="Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas rhoncus est magna euismod in dolor risus. Morbi nulla feugiat sit orci. Ac et velit sed phasellus mattis. Mauris imperdiet aenean convallis aliquet. Nunc vestibulum accumsan suspendisse dignissim magna. Sit cursus augue tortor mattis. Nullam sed ac tincidunt cras non"
-				/>
-
-				<FaqComponent
-					title="Technical"
-					question1="Who are we?"
-					content1="Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas rhoncus est magna euismod in dolor risus. Morbi nulla feugiat sit orci. Ac et velit sed phasellus mattis. Mauris imperdiet aenean convallis aliquet. Nunc vestibulum accumsan suspendisse dignissim magna. Sit cursus augue tortor mattis. Nullam sed ac tincidunt cras non"
-					question2="What do we do?"
-					content2="Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas rhoncus est magna euismod in dolor risus. Morbi nulla feugiat sit orci. Ac et velit sed phasellus mattis. Mauris imperdiet aenean convallis aliquet. Nunc vestibulum accumsan suspendisse dignissim magna. Sit cursus augue tortor mattis. Nullam sed ac tincidunt cras non"
-					question3="How long have we existed?"
-					content3="Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas rhoncus est magna euismod in dolor risus. Morbi nulla feugiat sit orci. Ac et velit sed phasellus mattis. Mauris imperdiet aenean convallis aliquet. Nunc vestibulum accumsan suspendisse dignissim magna. Sit cursus augue tortor mattis. Nullam sed ac tincidunt cras non"
-					question4="What benefits do i get from your service?"
-					content4="Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas rhoncus est magna euismod in dolor risus. Morbi nulla feugiat sit orci. Ac et velit sed phasellus mattis. Mauris imperdiet aenean convallis aliquet. Nunc vestibulum accumsan suspendisse dignissim magna. Sit cursus augue tortor mattis. Nullam sed ac tincidunt cras non"
-					question5="Who are we?"
-					content5="Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas rhoncus est magna euismod in dolor risus. Morbi nulla feugiat sit orci. Ac et velit sed phasellus mattis. Mauris imperdiet aenean convallis aliquet. Nunc vestibulum accumsan suspendisse dignissim magna. Sit cursus augue tortor mattis. Nullam sed ac tincidunt cras non"
-				/>
+				<FAQ />
 			</div>
 		</DefaultLayout>
 	);
 }
 
-export default faq;
+export default FaqPage;
