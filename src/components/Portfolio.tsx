213feb2066297405f5e8bbce3d5c5d287aba4f7e
Ojonugwa Alikali <alikali.ojonugwajustice@gmail.com>
2023-05-03 18:20:24 +0100

    Update the about-us codebase

--- src/components/Portfolio.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Portfolio() {
-	return (
-		<div className="h-[248px] w-[272px] rounded-lg border border-[#A0A0A0] bg-[#F5F5F5] lg:h-[363px] lg:w-[404px]"></div>
-	);
-}
-
-export default Portfolio;
--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -1,11 +1,10 @@
 import DefaultLayout from '@/layouts/DefaultLayout';
 import React from 'react';
 import Image from 'next/image';
-import mobilepreview from '../assets/images/mobilepreview.png';
+import mobilepreview1 from '../assets/images/mobile-prev1.png';
+import mobilepreview2 from '../assets/images/mobile-prev12.png';
 import businessmodel from '../assets/images/businessmodel.png';
-import Profile from '@/components/Profile';
-import Portfolio from '@/components/Portfolio';
-import polygon from '../assets/svg/polygon.svg';
+
 import Ellipsea from '../assets/images/Ellipsea.png';
 import Ellipseb from '../assets/images/Ellipseb.png';
 import Ellipsec from '../assets/images/Ellipsec.png';
@@ -14,21 +13,34 @@ import Ellipsee from '../assets/images/Ellipsee.png';
 import Ellipsef from '../assets/images/Ellipsef.png';
 import Ellipseg from '../assets/images/Ellipseg.png';
 import Ellipseh from '../assets/images/Ellipseh.png';
+import Polygon from '@/assets/svg/polygon';
 
-function Aboutus() {
+const profileData = [
+	{ id: 1, name: 'Kola Akindele', position: 'CEO', image: Ellipsea },
+	{ id: 2, name: 'Paul Okoye', position: 'CEO', image: Ellipseb },
+	{ id: 3, name: 'Kola Akindele', position: 'CEO', image: Ellipsec },
+	{ id: 4, name: 'Kola Akindele', position: 'CEO', image: Ellipsed },
+	{ id: 5, name: 'Kola Akindele', position: 'CEO', image: Ellipsee },
+	{ id: 6, name: 'Paul Okoye', position: 'CEO', image: Ellipsef },
+	{ id: 7, name: 'Kola Akindele', position: 'CEO', image: Ellipseg },
+	{ id: 8, name: 'Kola Akindele', position: 'CEO', image: Ellipseh }
+];
+
+function AboutUsPage() {
 	return (
 		<DefaultLayout>
 			<div className="mx-auto flex w-full flex-col">
 				<div className="mx-auto w-full lg:max-w-7xl">
-					<section>
-						<h1 className=" mx-auto mt-16 mb-5 max-w-max text-center text-4xl font-bold lg:text-5xl">
+					<section className="mt-20 mb-10 px-10 lg:mb-28 lg:mt-36">
+						<h1 className="mt-16 mb-5 text-center text-xl font-bold lg:text-4xl">
 							About Us
 						</h1>
-						<p className="mx-auto mt-5 text-center text-lg font-semibold text-[#aaa5a5] lg:max-w-2xl lg:text-3xl">
+						<p className="mx-auto mt-5 text-center text-lg  text-[#aaa5a5] lg:max-w-2xl lg:text-2xl">
 							A peak into who we are and what drives us to ease business
 							practices amongst MSMEs and SMEs
 						</p>
-						<p className="mx-auto mt-5 p-2 text-center text-lg font-normal lg:max-w-7xl lg:text-2xl">
+
+						<p className="mx-auto mt-10 text-center text-lg font-normal  lg:max-w-7xl lg:text-left lg:text-xl">
 							Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam
 							mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas
 							rhoncus est magna euismod in dolor risus. Morbi nulla feugiat sit
@@ -40,28 +52,39 @@ function Aboutus() {
 					</section>
 				</div>
 
-				<section className=" mt-7 flex h-[100px] w-full justify-around gap-5 bg-[#4C2884] lg:h-[320px] ">
-					<div className="ft-pattern" aria-hidden="true">
-						<Image src={polygon} alt="pattern" />
+				<section className=" relative mt-7 flex h-[100px] w-full justify-around gap-5 bg-[#4C2884] lg:h-[320px] ">
+					<div
+						className="absolute top-0 left-0 z-0 rotate-0"
+						aria-hidden="true"
+					>
+						<Polygon />
 					</div>
 
-					<div className="flex w-2/6 items-center justify-center">
-						<p className="xs:w-[172px] text-center text-base text-white lg:text-3xl">
-							Over <span className="text-[#FEC431]">50,000</span> businesses
-							enjoying tech services
-						</p>
-					</div>
-					<div className=" w-[150px] lg:w-2/6  ">
-						<Image src={mobilepreview} alt="" className="h-full" />
+					<div className="flex justify-between gap-x-3 px-10 lg:max-w-7xl">
+						<div className="relative z-10 flex w-1/2 items-center">
+							<p className="max-w-lg text-left text-base text-white lg:text-5xl ">
+								Over <span className="text-[#FEC431]">50,000</span> businesses
+								enjoying tech services
+							</p>
+						</div>
+						<div className="relative flex w-1/2 overflow-hidden">
+							<div className="flex items-end">
+								<Image src={mobilepreview1} alt="" className=" object-cover" />
+							</div>
+
+							<div className="flex items-start">
+								<Image src={mobilepreview2} alt="" className=" object-cover " />
+							</div>
+						</div>
 					</div>
 				</section>
 
-				<section className="my-2 mx-auto flex h-fit w-full flex-col gap-12 p-10 lg:max-w-[1440px] lg:flex-row lg:gap-36">
-					<div className="xs:w-full">
-						<h1 className="max-w-xs text-center text-4xl font-bold lg:text-5xl">
+				<section className="mx-auto flex flex-col-reverse gap-12 p-10 lg:mt-20 lg:max-w-7xl lg:flex-row lg:gap-36">
+					<div className="lg:w-3/5">
+						<h2 className="text-center text-xl font-bold lg:text-left lg:text-4xl">
 							Business Model
-						</h1>
-						<p className="mt-10 text-base font-normal lg:max-w-2xl lg:text-2xl">
+						</h2>
+						<p className="mt-10 text-lg text-[#A0A0A0] lg:text-xl">
 							Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam
 							mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas
 							rhoncus est magna euismod in dolor risus. Morbi nulla feugiat sit
@@ -69,8 +92,7 @@ function Aboutus() {
 							convallis aliquet. Nunc vestibulum accumsan suspendisse dignissim
 							magna. Sit cursus augue tortor mattis. Nullam sed ac tincidunt
 							cras non
-						</p>
-						<p className="mt-10 text-base font-normal lg:max-w-2xl lg:text-2xl">
+							<br /> <br />
 							Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam
 							mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas
 							rhoncus est magna euismod in dolor risus. Morbi nulla feugiat sit
@@ -80,72 +102,40 @@ function Aboutus() {
 							cras non
 						</p>
 					</div>
-					<div className=" mx-auto h-[248px] w-[272px] lg:h-[544px] lg:w-[465px] ">
-						<Image src={businessmodel} alt="" className="h-full" />
+
+					<div className="m-auto mt-7 lg:w-2/5">
+						<Image src={businessmodel} alt="" className="w-full object-cover" />
 					</div>
 				</section>
 
-				<section className="my-2 ">
-					<h1 className="mx-auto max-w-sm text-center text-3xl font-bold lg:text-4xl">
+				<section className="mx-auto mt-44 max-w-7xl px-10">
+					<h2 className="mx-auto mb-4 max-w-sm text-center text-xl font-bold lg:text-4xl">
 						Meet Our Team
-					</h1>
-					<div className="mx-auto flex max-w-7xl flex-row flex-wrap items-center justify-center justify-items-center gap-4 p-2 lg:gap-10 lg:p-10">
-						<Profile
-							teamname="Kola Akindele"
-							position="CEO"
-							alt="Kola Akindele"
-							image={Ellipsea}
-						/>
-						<Profile
-							teamname="Paul Okoye"
-							position="CEO"
-							alt="Paul Okoye"
-							image={Ellipseb}
-						/>
-						<Profile
-							teamname="Kola Akindele"
-							position="CEO"
-							alt="Kola Akindele"
-							image={Ellipsec}
-						/>
-						<Profile
-							teamname="Kola Akindele"
-							position="CEO"
-							alt="Kola Akindele"
-							image={Ellipsed}
-						/>
-						<Profile
-							teamname="Kola Akindele"
-							position="CEO"
-							alt="Kola Akindele"
-							image={Ellipsee}
-						/>
-						<Profile
-							teamname="Kola Akindele"
-							position="CEO"
-							alt="Kola Akindele"
-							image={Ellipsef}
-						/>
-						<Profile
-							teamname="Kola Akindele"
-							position="CEO"
-							alt="Kola Akindele"
-							image={Ellipseg}
-						/>
-						<Profile
-							teamname="Kola Akindele"
-							position="CEO"
-							alt="Kola Akindele"
-							image={Ellipseh}
-						/>
+					</h2>
+					<div className="flex flex-row flex-wrap items-center justify-center gap-4 p-2 lg:gap-x-10 lg:gap-y-24 lg:p-10">
+						{profileData.map((profile) => (
+							<div key={profile.id}>
+								<div className=" h-24 w-24 md:h-36 md:w-36 lg:h-64 lg:w-64">
+									<Image
+										src={profile.image}
+										alt={profile.name}
+										className="h-full w-full rounded-full"
+									/>
+								</div>
+								<div className="my-2 text-center">
+									<h2 className="font-bold">{profile.name}</h2>
+									<h3>{profile.position}</h3>
+								</div>
+							</div>
+						))}
 					</div>
 				</section>
 
-				<section className="my-10">
-					<h1 className="mx-auto max-w-[289px] text-center text-3xl font-bold lg:text-5xl">
+				<section className="my-10 mx-auto mt-24 px-10 lg:max-w-7xl">
+					<h2 className="mx-auto max-w-sm text-center text-xl font-bold lg:text-4xl">
 						Our Portfolio
-					</h1>
-					<p className="mx-auto mt-5 max-w-[1280px] p-2 text-center text-base lg:text-2xl">
+					</h2>
+					<p className="mx-auto mt-10 p-2 text-center text-lg font-normal  lg:max-w-7xl lg:text-left lg:text-xl">
 						Lorem ipsum dolor sit amet consectetur. Nisl morbi tellus diam
 						mauris. Ipsum diam cras magnis ac. Consequat feugiat egestas rhoncus
 						est magna euismod in dolor risus. Morbi nulla feugiat sit orci. Ac
@@ -153,10 +143,14 @@ function Aboutus() {
 						aliquet. Nunc vestibulum accumsan suspendisse dignissim magna. Sit
 						cursus augue tortor mattis. Nullam sed ac tincidunt cras non
 					</p>
-					<div className="m-3 mx-auto flex flex-col items-center justify-center gap-5 p-3 lg:flex-row lg:justify-around ">
-						<Portfolio />
-						<Portfolio />
-						<Portfolio />
+
+					<div className="m-3 mx-auto mt-36 flex flex-col items-center justify-center gap-24 p-3  lg:grid lg:grid-cols-3">
+						{Array.from({ length: 3 }, (_, i) => (
+							<div
+								key={i}
+								className="h-[248px] w-[272px] rounded-lg border border-[#A0A0A0] bg-[#F5F5F5] lg:max-h-[363px] lg:max-w-[404px]"
+							/>
+						))}
 					</div>
 				</section>
 			</div>
@@ -164,4 +158,4 @@ function Aboutus() {
 	);
 }
 
-export default Aboutus;
+export default AboutUsPage;
