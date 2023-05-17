b77279245a67cee563be0964e4b0873d150b25e5
Ojonugwa Alikali <alikali.ojonugwajustice@gmail.com>
2023-05-17 15:11:12 +0100

    Fix: Add SocialIcon

--- src/assets/svg/dasboard-menu/index.ts
+++ src/assets/svg/dasboard-menu/index.ts
@@ -3,7 +3,7 @@ export { default as ChartIcon } from './chartIcon';
 export { default as BudgetIcon } from './budgetIcon';
 export { default as SettingsIcon } from './settingsIcon';
 export { default as CalendarIcon } from './CalendarIcon';
-export { default as SocialIcon } from './SocialIcon2';
+export { default as SocialIcon } from './SocialIcon';
 export { default as SocialIcon2 } from './SocialIcon2';
 export { default as SignoutIcon } from './signoutIcon';
 export { default as EditIcon } from './editIcon';
